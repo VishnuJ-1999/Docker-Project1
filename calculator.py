e7045320eeb92ee02824b5215a4ee2e416ba44aa
VishnuJ-1999 <jvishnuprasad10@gmail.com>
2026-01-04 17:32:38 +0530

    First commit

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def mod(a,b):
+    return a%b
+while True:
+    a=int(input("enter a number"))
+    b=int(input("enter a number"))
+    c=input("enter operator")
+    if(c=="+"):
+        print(add(a,b))
+    elif(c=="-"):
+        print(sub(a,b))
+    elif(c=="*"):
+        print(mul(a,b))
+    elif(c=="/"):
+        print(div(a,b))
+    elif(c=="%"):
+        print(mod(a,b))
+    else:
+        print("invalid")
+    break
+
+# print("Happy new year.......welcome 2026.....wish you a great year ahead.....")
